fe9d9ddd4e16a64b06816f6ffa19c3730924d6f5
Atty9 <141070991+Atty9@users.noreply.github.com>
2024-05-08 13:40:45 +0000

    Simple valence analysis draft finished

--- cs50fp/backend.c
+++ cs50fp/backend.c
@@ -3,36 +3,70 @@
 #include <string.h>
 
 const int dataset_size = 2477; // known size of the csv
-const int hashtable_size = (int) (dataset_size / 0.75); // setting constant hash table size based
+const int hashtable_size = (int) (dataset_size / 0.75); // setting constant hash table size
+const char* text = "This company isn't good, I don't like it, it deceives, lies. Stock bad"; // placeholder text
 
-typedef struct vocab_entry {
-    char word[20]; // What would max length of a word be?
+typedef struct node {
+    char word[100]; 
     int valence;
-    struct vocab_entry* next; // check syntax
-} vocab_entry;
+    node* next; // syntax?
+} node;
 
-float SA_valence(char* text, vocab_entry dataset[]); // sentimen analysis valence func
-// other SA function
+void nullifier(node* hash_table[], int size);
+void table_filler(node* hash_table[], FILE* file);
+float valence_analyzer(char* text, node* dataset[]);
 int hash_func(char* word); // hash function
 
-// need to get target text(s) in here
-
-int main() 
+int main(void) 
 {
+    // Opening the dataset csv
     FILE *file = fopen("Afinn.csv", "r");
     if (file == NULL) {
         fprint("Failure opening the file\n");
         return 1;
     }
 
-    // Update to the new reality of hash table
-    vocab_entry hash_table[hashtable_size]; // creating hash table
-    char buffer[23]; // to store each line
-    int i = 0; // keeps track of line number
+    // Creating hash table
+    node* hash_table[hashtable_size];
+    nullifier(hash_table, hashtable_size); // sets pointers to NULL
+    table_filler(hash_table, file); // fills hash table with data from csv
+    
+    float valence = valence_analyzer(text, hash_table);
+    printf("Valence = %f", valence); // check syntax
 
-    while(fgets(buffer, sizeof(buffer), file))
+    fclose(file);
+
+    return 0;
+}
+
+void nullifier(node* hash_table[], int size)
+{
+    // sets hash_table pointers to NULL
+    for (int i = 0; i < size; i++)
     {
-        // implementing hash table
+        hash_table[i] = NULL;
+    }
+}
+
+void table_filler(node* hash_table[], FILE* file)
+{
+    /* Takes hash table and file as input.
+    Fills the table with data from the file (assumes csv) */
+
+    char buffer[100]; // stores line's contents
+    int i = 0; // stores line number
+
+    // Managing the first line of the csv
+    fgets(buffer, sizeof(buffer), file);
+    if (buffer == NULL)
+    {
+        printf("CSV is empty. Closing\n");
+        return 2;
+    }
+    // Reading the rest of csv line by line, copying data to the hashtable
+    while(fgets(buffer, sizeof(buffer), file)) // What about the title line of the csv?
+    {
+
         if (i >= dataset_size)
         {
             break;
@@ -44,52 +78,61 @@ int main()
             char* number = strtok(NULL, "\n"); // copies the number part as string
             if (number != NULL)
             {
-                int hash = hash_func(word)
-                // use while loop
-                // check if the current vocab_entry *next is NULL
-                // once it is, create next entry, set empty pointer ot it
-                if (hash_table[hash].next = NULL)
+                unsigned int hash = hash_func(word);
+                node* cursor = hash_table[hash];
+                while (cursor != NULL)
                 {
-                    // the code is not fully corect
-                    strcopy(hash_table[hash].word, word);
-                    hash_table[hash].valence = number;
-                    // hash_table[hash].next = pointer?
+                    cursor = cursor -> next;
                 }
-                else
-                {
-                    // follow the pointer and check again
-                }
+                strcopy(cursor -> word, word);
+                cursor -> valence = number;
+                cursor -> next = NULL;
             }
         }
-        i++
+        i++;
     }
-
-    int valence = SA_valence(text, dataset);
-
-    return 0;
 }
 
-float SA_valence(char* text, vocab_entry dataset[])
+float valence_analyzer(char* text, node* hash_table[])
 {
+    /* Takes a text as string and dictionary of words and corresponding valences as inputs.
+    Returns averange valence of text as a float between -5 and 5 */
 
-    // mind the length of words in dataset
-
-    int length = len(dataset);
     int total = 0;
+    const char* delimiters = " '\",.;:!-()#%@*$[]{}\n";
+    char* word = strtok(text, delimiters);
 
-    const char* delimiters = " '\",.;:!-()#@*$[]{}\%" // check espace syntax
+    // Check valence of the 1st word in the text
+    node* cursor = hash_table[hash_func(word)];
+    while (cursor != NULL)
+    {
+        cursor = cursor -> next;
+    }
+    total += cursor->valence;
 
-    // get total valence
-
-    // return total valence / i;
+    // Loop through the rest, getting all the valences
+    while ((word = strtok(NULL, delimiters)))
+    {
+        cursor = hash_table[hash_func(word)];
+        while (cursor != NULL)
+        {
+            cursor = cursor -> next;
+        }
+        total += cursor->valence;
+    }
+    
+    // return the average valence of the text
+    return total / hashtable_size;
 }
 
-int hash_func(char* word);
+int hash_func(char* word)
 {
+    // Takes a string, returns a hash (int)
+
     // credit this hash func, Polynomial Accumulation
     unsigned int hash = 0;
     int c;
-    while ((c = *word++)) // understand this better. Why the look stops?
+    while ((c = *(word++))) // while (*(word++)) also works, but we use c to dereference only once per loop
     {
         hash = hash * 33 + c;
     }
